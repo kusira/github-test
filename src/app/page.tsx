e7a2737b5f39a16de4bb5e1b65a80d769f53a170
kusira <takaaki200302@gmail.com>
2024-11-09 14:53:03 +0900

    やりなおし

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div>
       <div>Hello, yarinaoshi</div>
-<<<<<<< HEAD
       <div>AAAAAAAAAAAAAA</div>
       <div>AAAAAAAAAAAAAA</div>
       <div>AAAAAAAAAAAAAA</div>
@@ -19,8 +18,6 @@ export default function Home() {
       <div>AAAAAAAAAAAAAA</div>
       <div>AAAAAAAAAAAAAA</div>
       <div>AAAAAAAAAAAAAA</div>
-=======
->>>>>>> d45886cf0643e308a78a8ac46048c0ea0adef7e8
     </div>
   );
 }
